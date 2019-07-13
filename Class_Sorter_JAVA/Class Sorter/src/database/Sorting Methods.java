b5a494758885bec0569224c267fcf9330adedc69
chrishamlin98 <52384782+chrishamlin98@users.noreply.github.com>
2019-07-12 20:01:57 -0700

    Update Sorting Methods.java

--- Class_Sorter_JAVA/Class Sorter/src/database/Sorting Methods.java
+++ Class_Sorter_JAVA/Class Sorter/src/database/Sorting Methods.java
@@ -4,9 +4,12 @@ class Sorting Methods{
 
   public class ReverseSort {
     for (int i = 0; i < array.length / 2; i++) {
-            array b1 = array[i];
-            array[i] = array[array.length - 1 - i];
-            array[array.length - 1 - i] = b1;
+      array b1 = array[i];
+      array[i] = array[array.length - 1 - i];
+      array[array.length - 1 - i] = b1;
     }
+  }
+  public class AlphaSort {
+    
   }
 }

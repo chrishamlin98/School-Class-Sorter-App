79953da6e7554de27fa28f217e1e2506b1400aee
chrishamlin98 <52384782+chrishamlin98@users.noreply.github.com>
2019-07-17 06:17:35 -0700

    Update App.java

--- Class_Sorter_JAVA/Class Sorter/src/database/App.java
+++ Class_Sorter_JAVA/Class Sorter/src/database/App.java
@@ -1,3 +1,30 @@
+/*
+EmiliaToday at 5:35 AM
+
+Hmmm if I were to structure this one, I'd start off by having general
+interfaces that exhibit common database operations. It's up to you if you
+want this interface to be database agnostic (e.g: you can have mongodb
+implementations and such), but the simplest thing you can have would be:
+
+public interface DatabaseFind {
+    List<YourClassRepHere> findMultiple(Param param);
+    List<YourClassRepHere> findOne(Param param);
+}
+
+public interface DatabaseUpdate {
+    // filler
+}
+
+public interface DatabaseDelete {
+    // filler
+}
+
+and then you'll have specific implementation/s for these interfaces.
+You can also have another interface to segregate the specific querying role,
+like DatabaseQuery and have each of those interfaces above extend from it.
+Also pretty sure there are better interface names than what I have on the
+example, but I can't think of any atm so yeah, my apologies for that.
+
 package database;
 
 import javax.sql.DataSource;

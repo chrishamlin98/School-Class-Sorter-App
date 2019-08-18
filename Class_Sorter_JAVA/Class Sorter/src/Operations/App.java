0fbc47449ff6b7a4886e590a80c8dd9de5e0999c
chrishamlin98 <52384782+chrishamlin98@users.noreply.github.com>
2019-08-18 05:58:14 -0700

    Class moves and updates
    I moved some classes around and added a separate implementation package.  Then, I updated the DatabaseDelete class with proper methods.

--- Class_Sorter_JAVA/Class Sorter/src/Operations/App.java
+++ Class_Sorter_JAVA/Class Sorter/src/Operations/App.java
@@ -5,33 +5,32 @@ Hmmm if I were to structure this one, I'd start off by having general
 interfaces that exhibit common database operations. It's up to you if you
 want this interface to be database agnostic (e.g: you can have mongodb
 implementations and such), but the simplest thing you can have would be:
-
-public interface DatabaseFind {
-    List<YourClassRepHere> findMultiple(Param param);
-    List<YourClassRepHere> findOne(Param param);
-}
-
-public interface DatabaseUpdate {
-    // filler
-}
-
-public interface DatabaseDelete {
-    // filler
-}
-
-and then you'll have specific implementation/s for these interfaces.
-You can also have another interface to segregate the specific querying role,
-like DatabaseQuery and have each of those interfaces above extend from it.
-Also pretty sure there are better interface names than what I have on the
-example, but I can't think of any atm so yeah, my apologies for that.
-
-package database;
+*/
+package operations;
 
 import javax.sql.DataSource;
 
 public class App {
 
-public static void main(String[] args) {
+  public static void main(String[] args) {
+  }
 
-	}
+  public interface DatabaseRead {
+    List<YourClassRepHere> findMultiple(Param param);
+    List<YourClassRepHere> findOne(Param param);
+  }
+
+  public interface DatabaseUpdate {
+    // filler
+
+  }
+  Public interface DatabaseSort {
+
+  }
 }
+/*
+and then you'll have specific implementation/s for these interfaces.
+You can also have another interface to segregate the specific querying role,
+like DatabaseQuery and have each of those interfaces above extend from it.
+Also pretty sure there are better interface names than what I have on the
+example, but I can't think of any atm so yeah, my apologies for that.
---
+++ Class_Sorter_JAVA/Class Sorter/src/implementations/DatabaseDelete.java
@@ -0,0 +1,17 @@
+package implementaions;
+
+import operations;
+
+class Deletion implements DatabaseDelete
+{
+  public void deleteString();
+}
+  public void deleteString
+  {
+    System.out.println(deleteString + " has been deleted from the database");
+  }
+  public static void main(String[]args)
+  {
+  DatabaseDelete obj = new Deletion();
+  obj.deleteString();
+  }

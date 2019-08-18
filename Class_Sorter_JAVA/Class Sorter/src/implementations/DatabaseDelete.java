e90947d9318f7b86538a27f2efdb3d13eac231c2
chrishamlin98 <52384782+chrishamlin98@users.noreply.github.com>
2019-08-18 06:39:18 -0700

    Fixed method names
    swapped interface and class method names and fixed some visual mechs

--- Class_Sorter_JAVA/Class Sorter/src/implementations/DatabaseDelete.java
+++ Class_Sorter_JAVA/Class Sorter/src/implementations/DatabaseDelete.java
@@ -2,16 +2,18 @@ package implementaions;
 
 import operations;
 
+interface DatabaseRead
+{
+    public void deleteString();
+}
+
 class Deletion implements DatabaseDelete
 {
-  public void deleteString();
-}
-  public void deleteString
-  {
     System.out.println(deleteString + " has been deleted from the database");
-  }
-  public static void main(String[]args)
-  {
-  DatabaseDelete obj = new Deletion();
-  obj.deleteString();
-  }
+    
+    public static void main(String[]args)
+    {
+        DatabaseDelete obj = new Deletion();
+        obj.deleteString();
+    }
+}
